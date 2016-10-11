58287bd73d67659d883b89f4b73022d8adc1db96
Jose0PV <josepablov@gmail.com>
2016-10-11 02:42:22 -0600

    prueba python + astoxml

---
+++ prueba2.py
@@ -0,0 +1,15 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+    if n==10:
+        return factorial (0)
+
+print("Hello, World!")
+if (True):
+    print ("ey")
+else:
+    print ("A")
+while(False):
+    a = 0;
